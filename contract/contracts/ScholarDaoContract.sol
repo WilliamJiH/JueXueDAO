447fc216feb577b68ed140b3763cc4d013db3811
MingJin Lu <xia0weimo1999@gmail.com>
2022-09-09 22:03:17 +0800

    Finalize vote paper vote system, allow transition from scholar sand to scholar brick

--- contract/contracts/ScholarDaoContract.sol
+++ contract/contracts/ScholarDaoContract.sol
@@ -1,6 +1,6 @@
 // contracts/ScholarCoin.sol
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.1;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -29,10 +29,10 @@ contract PaperApprovalContract is Ownable{
 
     }
 
-    function recieveVote(bool vote) public{
+    function commitVote(bool vote) public{
         require(!contractIsClosed, "contract already closed!");
-        require(reviewers[msg.sender], "only reviewers can vote!");
-        reviewers[msg.sender] = vote;
+        require(reviewers[msg.sender], "You do not have the right to vote!");
+        reviewers[msg.sender] = false;
         if (vote){
             approvalCounts += 1;
         }
@@ -55,11 +55,9 @@ contract PaperApprovalContract is Ownable{
 
 contract ScholarDaoContract is ERC1155, Ownable {
     mapping (uint256 => string) private _uris;
-    uint256 public constant SCHOLA_BRICK = 0;
-    uint256 public constant SCHOLA_SAND = 1;
+    uint256 public constant SCHOLAR_BRICK = 0;
+    uint256 public constant SCHOLAR_SAND = 1;
 
-    // Mapping from token ID to account balances
-    mapping (uint256 => mapping(address => uint256)) private _balances;
     mapping (address => bool) public scholarAddresses;
 
     //a mapping of scholar and their papper 
@@ -67,14 +65,14 @@ contract ScholarDaoContract is ERC1155, Ownable {
 
     struct ScholarPaper {
         PaperApprovalContract paperContract;
-        mapping(address => bool) scholarSandClaimed;
+        mapping(address => bool) scholarSandClaimable;
         bool scholarBrickClaimed;
     }
 
     constructor() public ERC1155("") {
         // set up assets
-        _uris[SCHOLA_BRICK] = "SCHOLA_BRICK";
-        _uris[SCHOLA_SAND] = "SCHOLA_SAND";
+        _uris[SCHOLAR_BRICK] = "SCHOLA_BRICK";
+        _uris[SCHOLAR_SAND] = "SCHOLA_SAND";
 
     }
 
@@ -96,10 +94,6 @@ contract ScholarDaoContract is ERC1155, Ownable {
         scholarAddresses[recipient] = true;
     }
 
-    // function joinScholarDao(address recipient) public onlyOwner {
-    //     // to-do //contract become payable
-    // }
-
     function initReviewContract(address author, address paperUri, address[] memory _reviewers) public onlyOwner returns(address contractAddress){
         scholarPaperMap[author][paperUri].paperContract =  new PaperApprovalContract(msg.sender, author, paperUri, _reviewers);
         scholarPaperMap[author][paperUri].scholarBrickClaimed = false;
@@ -110,21 +104,31 @@ contract ScholarDaoContract is ERC1155, Ownable {
         require(address(scholarPaperMap[recipient][paperUri].paperContract) != address(0x0), "Recipient must own this paper");
         require(scholarPaperMap[recipient][paperUri].paperContract.isContractClaimable(), "Paper contract cannot be claimed!");
         require(msg.sender == recipient || msg.sender == owner(), "Only the author or the owner can claim this contract!");
-        _mint(recipient, SCHOLA_BRICK, 1, "");
+        require(!scholarPaperMap[recipient][paperUri].scholarBrickClaimed, "Scholar brick was claimed!");
+        _mint(recipient, SCHOLAR_BRICK, 1, "");
+        scholarPaperMap[recipient][paperUri].scholarBrickClaimed = true;
     }
 
     function addPaperRefferenceAddresses(address author, address paperUri, address[] memory referenceScholarAddresses) public onlyOwner{
         for(uint i = 0; i < referenceScholarAddresses.length; i ++){
-            scholarPaperMap[author][paperUri].scholarSandClaimed[referenceScholarAddresses[i]] = false;
+            scholarPaperMap[author][paperUri].scholarSandClaimable[referenceScholarAddresses[i]] = true;
         }
     }
 
     function mintScholarSand(address recipient, address author, address paperUri) public{
-        require(address(scholarPaperMap[recipient][paperUri].paperContract) != address(0x0), "recipient must be a scholar!");
-        require(scholarPaperMap[author][paperUri].scholarSandClaimed[recipient] == false, "recipient is unable to claim with this paper");
-        _mint(recipient, SCHOLA_SAND, 5, "");
-        scholarPaperMap[author][paperUri].scholarSandClaimed[recipient] = true;
+        require(address(scholarPaperMap[author][paperUri].paperContract) != address(0x0), "Paper must exists");
+        require(scholarPaperMap[author][paperUri].scholarSandClaimable[recipient], "Recipient is unable to claim with this paper");
+        _mint(recipient, SCHOLAR_SAND, 5, "");
+        scholarPaperMap[author][paperUri].scholarSandClaimable[recipient] = false;
 
+    }
+
+    function convertSandToBrick(address recipient, uint amount) public{
+        // convert every 1 thousand sand from msg.sender into 1 brick to recipient
+        require(balanceOf(msg.sender, SCHOLAR_SAND) >= amount, "Sender must have enough sand amount");
+        uint convertNum = amount / 1000;
+        _burn(msg.sender, SCHOLAR_SAND, convertNum * 1000);
+        _mint(recipient, SCHOLAR_BRICK, convertNum, "");
     }
 
 
