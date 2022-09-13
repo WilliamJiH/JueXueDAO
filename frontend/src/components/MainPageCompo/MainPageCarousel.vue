803fbc36a9e90e08a14c859bc060046ae9ac48dc
MingJin Lu <xia0weimo1999@gmail.com>
2022-09-13 16:17:24 +0800

    main page added carousel and publish buttons

---
+++ frontend/src/components/MainPageCompo/MainPageCarousel.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <b-carousel
+      id="carousel-1"
+      v-model="slide"
+      :interval="4000"
+      controls
+      indicators
+      background="#ffffff00"
+      img-width="1024"
+      img-height="480"
+      style="text-shadow: 1px 1px 2px #333"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+
+      <!-- Slides with img slot -->
+      <b-carousel-slide>
+        <template #img>
+          <img
+            class="d-block img-fluid w-100"
+            width="1024"
+            height="480"
+            src="../../assets/images/main_carousel_slide1.png"
+            alt="image slot"
+          />
+        </template>
+      </b-carousel-slide>
+       <b-carousel-slide>
+        <template #img>
+          <img
+            class="d-block img-fluid w-100"
+            width="1024"
+            height="480"
+            src="../../assets/images/main_carousel_slide2.png"
+            alt="image slot"
+          />
+        </template>
+      </b-carousel-slide>
+       <b-carousel-slide>
+        <template #img>
+          <img
+            class="d-block img-fluid w-100"
+            width="1024"
+            height="480"
+            src="../../assets/images/main_carousel_slide3.png"
+            alt="image slot"
+          />
+        </template>
+      </b-carousel-slide>
+    </b-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainPageCarousel',
+  data() {
+    return {
+      slide: 0,
+      sliding: null,
+    }
+  },
+  methods: {
+    onSlideStart(slide) {
+      this.sliding = true
+    },
+    onSlideEnd(slide) {
+      this.sliding = false
+    },
+  },
+}
+</script>
+
+<style>
+.carousel-inner {
+    border-radius: 1rem;
+}
+</style>
--- frontend/src/pages/MainPage.vue
+++ frontend/src/pages/MainPage.vue
@@ -1,13 +1,77 @@
 <template>
-  <div>Hello Main Page</div>
+  <div>
+    <div class="main-searchbar input-group">
+      <input
+        type="search"
+        class="form-control"
+        placeholder="Search"
+        aria-label="Search"
+        aria-describedby="search-addon"
+        v-model="searchText"
+        style="border-radius: 1rem"
+      />
+    </div>
+    <!-- category selector -->
+    <div class="category-selector text-center">
+      <b-form-radio-group
+        v-model="selected"
+        :options="options"
+        name="radio-inline"
+      ></b-form-radio-group>
+    </div>
+
+    <div class="row m-3">
+      <div class="col-md-8 pr-2">
+        <MainPageCarousel />
+      </div>
+      <div
+        class="col-md-4 pl-2"
+        style="
+          display: flex;
+          flex-direction: column;
+          justify-content: space-around;
+        "
+      >
+        <b-button
+          variant="primary"
+          style="height: 40%; font-size: 2rem; font-weight: 600"
+          >投稿</b-button
+        >
+        <b-button
+          variant="primary"
+          style="height: 30%; font-size: 1.5rem; font-weight: 400"
+          >成为审稿人</b-button
+        >
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import MainPageCarousel from '../components/MainPageCompo/MainPageCarousel.vue'
 export default {
-
+  name: 'MainPage',
+  components: { MainPageCarousel },
+  data() {
+    return {
+      searchText: '',
+      selected: 'lunwen',
+      options: [
+        { text: '学术论文', value: 'lunwen' },
+        { text: '期刊', value: 'qikan' },
+        { text: '专利', value: 'zhuanli' },
+        { text: '文件', value: 'wenjian' },
+        { text: '图书', value: 'tushu' },
+      ],
+    }
+  },
 }
 </script>
 
-<style>
-
+<style scoped>
+.main-searchbar {
+  margin: 1rem 3rem;
+  border-radius: 3rem;
+  width: auto;
+}
 </style>
